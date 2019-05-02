f846a04e3fbde8295fe30559c2f87e3b74306477
mehamasum <mehamasum@gmail.com>
2019-05-02 23:28:24 +0600

    refactor: remove unnecessary stuff from navbar

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -7,10 +7,6 @@ export default theme => ({
   grow: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
   title: {
     display: 'none',
     [theme.breakpoints.up('sm')]: {
